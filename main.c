f8a74ad589b4dc03448c6f05a662b6c2001afd9e
Fernando Cesar <cmnando.silva@gmail.com>
2021-03-22 17:45:47 -0300

    program has ended

--- main.c
+++ main.c
@@ -5,126 +5,162 @@
 esse erro ocorre quando os métodos get e set são invocados com um índice maior 
 ou igual ao parâmetro N da classe. 
 */
-class biggerError : public  std::exception{
+class biggerError : public std::exception
+{
 public:
-  virtual const char *errorMessage(){
-    return "Erro: indice maior que arranjo.";
-  }
+    virtual const char *errorMessage()
+    {
+        return "Erro: indice maior que arranjo.";
+    }
 };
 /*
  esse erro ocorre quando os métodos get e set são invocados com um índice 
  negativo. 
 */
-class negativeError : public  std::exception{
+class negativeError : public std::exception
+{
 public:
-  virtual const char *errorMessage(){
-    return "Erro: indice negativo.";
-  }
+    virtual const char *errorMessage()
+    {
+        return "Erro: indice negativo.";
+    }
 };
-template <class T, int N> class BoundedArray {
-  public:
-    void set(int index, T value) {
-      buf[index] = value;
+template <class T, int N>
+class BoundedArray
+{
+public:
+    void set(int index, T value)
+    {
+        buf[index] = value;
     }
-    T get(int index) {
-      return buf[index];
+    T get(int index)
+    {
+        return buf[index];
     }
-  private:
+
+private:
     T buf[N];
 };
 /*
 esse erro ocorre quando os métodos get e set são invocados com um índice maior
 ou igual ao parâmetro N da classe.
 */
-class uninitializedError : public std::exception{
+class uninitializedError : public std::exception
+{
 public:
-  virtual const char *errorMessage(){
-    return "Erro: indice nao inicializado.";
-  }
+    virtual const char *errorMessage()
+    {
+        return "Erro: indice nao inicializado.";
+    }
 };
-template <class T, int N> class BoundedArray{
+template <class T, int N>
+class BoundedArray
+{
 private:
-  T buf[N];
-  std::vector<int> uninitialized;
+    T buf[N];
+    std::vector<int> uninitialized;
+
 public:
-  BoundedArray(){
-    for (int i = 0; i <= N-1; i++){
-      uninitialized.push_back(0);
+    BoundedArray()
+    {
+        for (int i = 0; i <= N - 1; i++)
+        {
+            uninitialized.push_back(0);
+        }
     }
-  }
-  T get(int posicao){
-    if (!(posicao >= 0)){
-      throw negativeError();  
+    T get(int posicao)
+    {
+        if (!(posicao >= 0))
+        {
+            throw negativeError();
+        }
+        else if (!(posicao < N))
+        {
+            throw biggerError();
+        }
+        else if (uninitialized[posicao] == 0)
+        {
+            throw uninitializedError();
+        }
+        else
+        {
+            return buf[posicao];
+        }
     }
-    else if (!(posicao < N)){
-      throw biggerError(); 
+    void set(int posicao, T valor)
+    {
+        if (posicao >= 0 && posicao < N)
+        {
+            uninitialized[posicao]++;
+            buf[posicao] = valor;
+        }
+        else if (!(posicao >= 0))
+        {
+            throw negativeError();
+        }
+        else
+        {
+            throw biggerError();
+        }
     }
-    else if (uninitialized[posicao] == 0){
-      throw uninitializedError();
-    }
-    else{
-      return buf[posicao];   
-    }
-  }
-  void set(int posicao, T valor){
-    if (posicao >= 0 && posicao < N){
-        uninitialized[posicao]++; 
-        buf[posicao] = valor;
-    }
-    else if (!(posicao >= 0)){
-      throw negativeError();  
-    }
-    else{
-      throw biggerError();  
-    }
-  }
 };
 
 template <class T>
-void testArray(){
-  BoundedArray<T, 8> a;
-  char action;
-  while (std::cin >> action){
-    int index;
-    std::cin >> index;
-    try{
-      if (action == 's'){
-        T value;
-        std::cin >> value;
-        a.set(index, value);
-      }
-      else if (action == 'g'){
-        std::cout << a.get(index) << std::endl;
-      }
+void testArray()
+{
+    BoundedArray<T, 8> a;
+    char action;
+    while (std::cin >> action)
+    {
+        int index;
+        std::cin >> index;
+        try
+        {
+            if (action == 's')
+            {
+                T value;
+                std::cin >> value;
+                a.set(index, value);
+            }
+            else if (action == 'g')
+            {
+                std::cout << a.get(index) << std::endl;
+            }
+        }
+        catch (biggerError e)
+        {
+            std::cerr << e.errorMessage();
+        }
+        catch (negativeError e)
+        {
+            std::cerr << e.errorMessage();
+        }
+        catch (uninitializedError e)
+        {
+            std::cerr << e.errorMessage();
+        }
+        catch (...)
+        {
+            std::cerr << "Erro inesperado!";
+        }
     }
-    catch (biggerError e){
-       std::cerr << e.errorMessage();
-    }
-    catch (negativeError e){
-       std::cerr << e.errorMessage();
-    }
-    catch (uninitializedError e){
-       std::cerr << e.errorMessage();
-    }
-    catch (...){
-       std::cerr << "Erro inesperado!";
-    }
-  }
 }
 
-int main() {
-  char type;
-  std::cin >> type;
-  switch(type) {
+int main()
+{
+    char type;
+    std::cin >> type;
+    switch (type)
+    {
     case 'd':
-      testArray<double>();
-      break;
+        testArray<double>();
+        break;
     case 'i':
-      testArray<int>();
-      break;
+        testArray<int>();
+        break;
     case 's':
-      testArray<std::string>();
-      break;
-  }
-  return 0;
+        testArray<std::string>();
+        break;
+    }
+    return 0;
 }
